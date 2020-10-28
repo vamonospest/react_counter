f42a0e61ce61ed56c8ecb61bd523c29d51d50866
Dmitry Morozov <10mdv11@gmail.com>
2020-10-29 01:56:21 +0200

    Updates

---
+++ precache-manifest.7473493e85a746449341c7ecb4287373.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8178f0d3e14ba04e2b1a52f5f4f5a155",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f78df303ce472f99f035",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "7c7a19eaf806aa4e071d",
+    "url": "./static/js/2.cad20d42.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.cad20d42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f78df303ce472f99f035",
+    "url": "./static/js/main.f919805f.chunk.js"
+  },
+  {
+    "revision": "2a286f7172708a7e8c5a",
+    "url": "./static/js/runtime-main.3a2c9fb2.js"
+  }
+]);
